4306cd845076271dae402d38f75f7021c77130dc
Aaron Miller <millrfz@amazon.com>
2023-06-05 12:17:22 -0700

    update: `ack-apigatewayv2-controller` to v1.0.3

--- pkg/application/ack/apigatewayv2_controller/apigatewayv2_controller.go
+++ pkg/application/ack/apigatewayv2_controller/apigatewayv2_controller.go
@@ -15,7 +15,7 @@ import (
 // Helm:    https://github.com/aws-controllers-k8s/apigatewayv2-controller/tree/main/helm
 // Chart:   https://gallery.ecr.aws/aws-controllers-k8s/apigatewayv2-chart
 // Repo:    https://gallery.ecr.aws/aws-controllers-k8s/apigatewayv2-controller
-// Version: Latest is v0.1.4 (as of 10/24/22)
+// Version: Latest is v1.0.3 (as of 6/11/23)
 
 func NewApp() *application.Application {
 	app := &application.Application{
@@ -44,10 +44,10 @@ func NewApp() *application.Application {
 			Namespace:      "ack-system",
 			ServiceAccount: "ack-apigatewayv2-controller",
 			DefaultVersion: &application.LatestPrevious{
-				LatestChart:   "v0.1.4",
-				Latest:        "v0.1.4",
-				PreviousChart: "v0.1.2",
-				Previous:      "v0.1.2",
+				LatestChart:   "1.0.3",
+				Latest:        "1.0.3",
+				PreviousChart: "v0.1.4",
+				Previous:      "v0.1.4",
 			},
 		},
 
