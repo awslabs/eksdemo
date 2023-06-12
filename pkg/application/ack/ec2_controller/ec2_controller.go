5841d905d6ecd9722b9560050bd38795d7da191c
Aaron Miller <1521726+aaroniscode@users.noreply.github.com>
2023-06-11 18:56:07 -0700

    update: `ack-ec2-controller` to v1.0.3 (#29)

--- pkg/application/ack/ec2_controller/ec2_controller.go
+++ pkg/application/ack/ec2_controller/ec2_controller.go
@@ -15,7 +15,7 @@ import (
 // Helm:    https://github.com/aws-controllers-k8s/ec2-controller/tree/main/helm
 // Chart:   https://gallery.ecr.aws/aws-controllers-k8s/ec2-chart
 // Repo:    https://gallery.ecr.aws/aws-controllers-k8s/ec2-controller
-// Version: Latest is v0.0.21 (as of 10/24/22)
+// Version: Latest is v1.0.3 (as of 6/11/23)
 
 func NewApp() *application.Application {
 	app := &application.Application{
@@ -41,10 +41,10 @@ func NewApp() *application.Application {
 			Namespace:      "ack-system",
 			ServiceAccount: "ack-ec2-controller",
 			DefaultVersion: &application.LatestPrevious{
-				LatestChart:   "v0.0.21",
-				Latest:        "v0.0.21",
-				PreviousChart: "v0.0.15",
-				Previous:      "v0.0.15",
+				LatestChart:   "1.0.3",
+				Latest:        "1.0.3",
+				PreviousChart: "v0.0.21",
+				Previous:      "v0.0.21",
 			},
 		},
 
