f8a12738eeca32fe3d1bce06058a2297191e6b58
Aaron Miller <1521726+aaroniscode@users.noreply.github.com>
2023-06-11 19:11:24 -0700

    update: `ack-ecr-controller` to v1.0.4 (#30)

--- pkg/application/ack/ecr_controller/ecr_controller.go
+++ pkg/application/ack/ecr_controller/ecr_controller.go
@@ -13,8 +13,9 @@ import (
 // Docs:    https://aws-controllers-k8s.github.io/community/reference/
 // GitHub:  https://github.com/aws-controllers-k8s/ecr-controller
 // Helm:    https://github.com/aws-controllers-k8s/ecr-controller/tree/main/helm
+// Chart:   https://gallery.ecr.aws/aws-controllers-k8s/ecr-chart
 // Repo:    https://gallery.ecr.aws/aws-controllers-k8s/ecr-controller
-// Version: Latest is v0.1.7 (as of 10/24/22)
+// Version: Latest is v1.0.4 (as of 6/11/23)
 
 func NewApp() *application.Application {
 	app := &application.Application{
@@ -40,10 +41,10 @@ func NewApp() *application.Application {
 			Namespace:      "ack-system",
 			ServiceAccount: "ack-ecr-controller",
 			DefaultVersion: &application.LatestPrevious{
-				LatestChart:   "v0.1.7",
-				Latest:        "v0.1.7",
-				PreviousChart: "v0.1.3",
-				Previous:      "v0.1.3",
+				LatestChart:   "1.0.4",
+				Latest:        "1.0.4",
+				PreviousChart: "v0.1.7",
+				Previous:      "v0.1.7",
 			},
 		},
 
