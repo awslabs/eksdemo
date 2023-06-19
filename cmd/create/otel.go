150b3930ac99001c7d07a53d0389c35ba3442466
Aaron Miller <millrfz@amazon.com>
2023-06-19 14:39:49 -0700

    add: `create otel-simplest` and `create otel-prometheus-cloudwatch`

---
+++ cmd/create/otel.go
@@ -0,0 +1,33 @@
+package create
+
+import (
+	"github.com/awslabs/eksdemo/pkg/resource"
+	"github.com/awslabs/eksdemo/pkg/resource/otel"
+	"github.com/spf13/cobra"
+)
+
+var otelCollectors []func() *resource.Resource
+
+func NewOtelCollectorCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "otel-collector",
+		Short:   "Open Telemetry (OTEL) Collectors",
+		Aliases: []string{"otel"},
+	}
+
+	// Don't show flag errors for `create otel-collector` without a subcommand
+	cmd.DisableFlagParsing = true
+
+	for _, r := range otelCollectors {
+		cmd.AddCommand(r().NewCreateCmd())
+	}
+
+	return cmd
+}
+
+func init() {
+	otelCollectors = []func() *resource.Resource{
+		otel.NewPrometheusCloudWatchCollector,
+		otel.NewSimplestCollector,
+	}
+}
---
+++ pkg/resource/otel/simplest.go
@@ -0,0 +1,63 @@
+package otel
+
+import (
+	"github.com/awslabs/eksdemo/pkg/cmd"
+	"github.com/awslabs/eksdemo/pkg/manifest"
+	"github.com/awslabs/eksdemo/pkg/resource"
+	"github.com/awslabs/eksdemo/pkg/template"
+)
+
+func NewSimplestCollector() *resource.Resource {
+	return &resource.Resource{
+		Command: cmd.Command{
+			Name:        "simplest",
+			Description: "Simplest Collector",
+			Args:        []string{"NAME"},
+		},
+
+		Manager: &manifest.ResourceManager{
+			Template: &template.TextTemplate{
+				Template: simplestCollectorTemplate,
+			},
+		},
+
+		Options: &resource.CommonOptions{
+			Namespace:     "default",
+			NamespaceFlag: true,
+		},
+	}
+}
+
+// https://github.com/open-telemetry/opentelemetry-operator#getting-started
+const simplestCollectorTemplate = `---
+apiVersion: opentelemetry.io/v1alpha1
+kind: OpenTelemetryCollector
+metadata:
+  namespace: {{ .Namespace }}
+  name: {{ .Name }}
+spec:
+  config: |
+    receivers:
+      otlp:
+        protocols:
+          grpc:
+          http:
+    processors:
+      memory_limiter:
+        check_interval: 1s
+        limit_percentage: 75
+        spike_limit_percentage: 15
+      batch:
+        send_batch_size: 10000
+        timeout: 10s
+
+    exporters:
+      logging:
+
+    service:
+      pipelines:
+        traces:
+          receivers: [otlp]
+          processors: []
+          exporters: [logging]
+`
