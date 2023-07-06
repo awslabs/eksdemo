70ff4e3a669ac6526530dd14ba93ee936aa1aa7b
Aaron Miller <millrfz@amazon.com>
2023-07-06 09:31:17 -0700

    fix: `get-alarm` to include composite alarms

--- pkg/resource/alarm/printer.go
+++ pkg/resource/alarm/printer.go
@@ -5,30 +5,41 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 	"github.com/awslabs/eksdemo/pkg/printer"
 	"github.com/hako/durafmt"
 )
 
 type AlarmPrinter struct {
-	alarms []types.MetricAlarm
+	alarms Alarms
 }
 
-func NewNamespacePrinter(metrics []types.MetricAlarm) *AlarmPrinter {
-	return &AlarmPrinter{metrics}
+func NewAlarmPrinter(alarms Alarms) *AlarmPrinter {
+	return &AlarmPrinter{alarms}
 }
 
 func (p *AlarmPrinter) PrintTable(writer io.Writer) error {
 	table := printer.NewTablePrinter()
-	table.SetHeader([]string{"Updated", "State", "Name"})
+	table.SetHeader([]string{"Updated", "State", "Name", "Type"})
 
-	for _, a := range p.alarms {
+	for _, a := range p.alarms.CompositeAlarms {
 		updated := durafmt.ParseShort(time.Since(aws.ToTime(a.StateUpdatedTimestamp)))
 
 		table.AppendRow([]string{
 			updated.String(),
 			string(a.StateValue),
 			aws.ToString(a.AlarmName),
+			"Composite",
+		})
+	}
+
+	for _, a := range p.alarms.MetricAlarms {
+		updated := durafmt.ParseShort(time.Since(aws.ToTime(a.StateUpdatedTimestamp)))
+
+		table.AppendRow([]string{
+			updated.String(),
+			string(a.StateValue),
+			aws.ToString(a.AlarmName),
+			"Metric",
 		})
 	}
 
