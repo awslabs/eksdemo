41decde0c8c1fd3d0ac3fffabf5e0dee89ef610f
Sébastien Allamand <sallaman@amazon.com>
2024-02-12 17:23:49 +0100

    Bump vpc-lattive to v1.0.3

--- pkg/application/vpc_lattice_controller/vpc_lattice_controller.go
+++ pkg/application/vpc_lattice_controller/vpc_lattice_controller.go
@@ -63,11 +63,35 @@ Statement:
 - Effect: Allow
   Action:
   - vpc-lattice:*
-  - iam:CreateServiceLinkedRole
   - ec2:DescribeVpcs
   - ec2:DescribeSubnets
   - ec2:DescribeTags
+  - ec2:DescribeSecurityGroups
+  - logs:CreateLogDelivery
+  - logs:GetLogDelivery
+  - logs:DescribeLogGroups
+  - logs:PutResourcePolicy
+  - logs:DescribeResourcePolicies
+  - logs:UpdateLogDelivery
+  - logs:DeleteLogDelivery
+  - logs:ListLogDeliveries
+  - tag:GetResources
+  - firehose:TagDeliveryStream
+  - s3:GetBucketPolicy
+  - s3:PutBucketPolicy
   Resource: "*"
+- Effect: Allow
+  Action: iam:CreateServiceLinkedRole
+  Resource: arn:aws:iam::*:role/aws-service-role/vpc-lattice.amazonaws.com/AWSServiceRoleForVpcLattice
+  Condition:
+    StringLike:
+      iam:AWSServiceName: vpc-lattice.amazonaws.com
+- Effect: Allow
+  Action: iam:CreateServiceLinkedRole
+  Resource: arn:aws:iam::*:role/aws-service-role/delivery.logs.amazonaws.com/AWSServiceRoleForLogDelivery
+  Condition:
+    StringLike:
+      iam:AWSServiceName: delivery.logs.amazonaws.com
 `
 
 const valuesTemplate = `---
