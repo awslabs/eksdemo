9b2a43a9da46f795dec0d6c422c73eb769b341e5
Aaron Miller <millrfz@amazon.com>
2024-07-25 09:05:05 -0700

    add: `crossplane-ec2-provider`

--- cmd/install/crossplane.go
+++ cmd/install/crossplane.go
@@ -3,6 +3,7 @@ package install
 import (
 	"github.com/awslabs/eksdemo/pkg/application"
 	"github.com/awslabs/eksdemo/pkg/application/crossplane/core"
+	"github.com/awslabs/eksdemo/pkg/application/crossplane/ec2"
 	"github.com/awslabs/eksdemo/pkg/application/crossplane/s3"
 	"github.com/spf13/cobra"
 )
@@ -46,6 +47,7 @@ func NewUninstallCrossplaneCmd() *cobra.Command {
 func init() {
 	crossplane = []func() *application.Application{
 		core.NewApp,
+		ec2.NewApp,
 		s3.NewApp,
 	}
 }
