1be784375bc0e10c26d77a749e46bcc535054527
Aaron Miller <1521726+aaroniscode@users.noreply.github.com>
2024-07-25 08:02:42 -0700

    add: `create crossplane-s3-bucket` (#198)

---
+++ cmd/create/crossplane.go
@@ -0,0 +1,32 @@
+package create
+
+import (
+	"github.com/awslabs/eksdemo/pkg/resource"
+	"github.com/awslabs/eksdemo/pkg/resource/crossplane/s3"
+	"github.com/spf13/cobra"
+)
+
+var crossplane []func() *resource.Resource
+
+func NewCrossplaneCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "crossplane",
+		Short:   "The Cloud Native Control Plane",
+		Aliases: []string{"cp"},
+	}
+
+	// Don't show flag errors for `create crossplane` without a subcommand
+	cmd.DisableFlagParsing = true
+
+	for _, r := range crossplane {
+		cmd.AddCommand(r().NewCreateCmd())
+	}
+
+	return cmd
+}
+
+func init() {
+	crossplane = []func() *resource.Resource{
+		s3.NewResource,
+	}
+}
