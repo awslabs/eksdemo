f849b203ca3dfa35e0fbda785c8d657e89fb67f3
Aaron Miller <millrfz@amazon.com>
2024-09-08 18:25:28 -0700

    rebase

--- pkg/application/argo/cd/argo_cd.go
+++ pkg/application/argo/cd/argo_cd.go
@@ -1,4 +1,4 @@
-package argo_cd
+package cd
 
 import (
 	"github.com/awslabs/eksdemo/pkg/application"
@@ -11,7 +11,7 @@ import (
 // GitHub:  https://github.com/argoproj/argo-cd
 // Helm:    https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd
 // Repo:    quay.io/argoproj/argocd
-// Version: Latest Chart is 5.37.0, Argo CD v2.7.7 (as of 7/5/23)
+// Version: Latest Chart is 7.5.2, Argo CD v2.12.3 (as of 9/8/24)
 
 func NewApp() *application.Application {
 	app := &application.Application{
@@ -40,36 +40,38 @@ fullnameOverride: argocd
 global:
   image:
     tag: {{ .Version }}
+configs:
+{{- if .IngressHost }}
+  params:
+    server.insecure: true
+{{- end }}
+  secret:
+    # -- Bcrypt hashed admin password
+    argocdServerAdminPassword: "{{ .AdminPassword | bcrypt }}"
 server:
   service:
     type: {{ .ServiceType }}
 {{- if .IngressHost }}
-  extraArgs:
-    - --insecure
   ingress:
     enabled: true
+  {{- if eq .IngressClass "alb" }}
+    controller: aws
+    aws:
+      serviceType: {{ .ServiceType }}
+  {{- end }}
     annotations:
       {{- .IngressAnnotations | nindent 6 }}
     ingressClassName: {{ .IngressClass }}
-    hosts:
-    - {{ .IngressHost }}
-    tls:
+    hostname: {{ .IngressHost }}
+    extraTls:
     - hosts:
       - {{ .IngressHost }}
     {{- if ne .IngressClass "alb" }}
       secretName: argocd-server-tls
     {{- end}}
   {{- if eq .IngressClass "alb" }}
-  ingressGrpc:
-    enabled: true
-    isAWSALB: true
-    awsALB:
-      serviceType: ClusterIP
-      backendProtocolVersion: GRPC
+  #ingressGrpc:
+  #  enabled: true
   {{- end }}
 {{- end }}
-configs:
-  secret:
-    # -- Bcrypt hashed admin password
-    argocdServerAdminPassword: "{{ .AdminPassword | bcrypt }}"
 `
--- pkg/application/argo/cd/options.go
+++ pkg/application/argo/cd/options.go
@@ -1,4 +1,4 @@
-package argo_cd
+package cd
 
 import (
 	"github.com/awslabs/eksdemo/pkg/application"
@@ -15,10 +15,10 @@ func newOptions() (options *ArgoCdOptions, flags cmd.Flags) {
 	options = &ArgoCdOptions{
 		ApplicationOptions: application.ApplicationOptions{
 			DefaultVersion: &application.LatestPrevious{
-				LatestChart:   "6.10.0",
-				Latest:        "v2.11.4",
-				PreviousChart: "5.37.0",
-				Previous:      "v2.7.7",
+				LatestChart:   "7.5.2",
+				Latest:        "v2.12.3",
+				PreviousChart: "7.5.2",
+				Previous:      "v2.12.3",
 			},
 			DisableServiceAccountFlag:    true,
 			ExposeIngressAndLoadBalancer: true,
